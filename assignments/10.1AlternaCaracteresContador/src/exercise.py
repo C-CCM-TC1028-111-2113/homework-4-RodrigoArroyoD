68343a150b75b5bdf699398a241f66cf5f36b6b0
RodrigoArroyoD <88851208+RodrigoArroyoD@users.noreply.github.com>
2021-09-04 12:06:55 -0500

    Update exercise.py

--- assignments/10.1AlternaCaracteresContador/src/exercise.py
+++ assignments/10.1AlternaCaracteresContador/src/exercise.py
@@ -1,16 +1,16 @@
 def main():
     #escribe tu código abajo de esta línea
     j=1
-n=int(input())
-while j<=n:
-    print(str(j)+'.-#')
-    if j==n:
-        break
-    j=j+1
-    print(str(j)+'.-%')
-    if j==n:
-        break
-    j=j+1
+    n=int(input())
+    while j<=n:
+        print(str(j)+'.-#')
+        if j==n:
+            break
+        j=j+1
+        print(str(j)+'.-%')
+        if j==n:
+            break
+        j=j+1
     
 
     pass
