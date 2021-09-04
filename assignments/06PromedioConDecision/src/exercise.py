e419f11f0fdfb9b6db31930c765f6aedd47d17cc
RodrigoArroyoD <88851208+RodrigoArroyoD@users.noreply.github.com>
2021-09-04 12:06:14 -0500

    Update exercise.py

--- assignments/06PromedioConDecision/src/exercise.py
+++ assignments/06PromedioConDecision/src/exercise.py
@@ -1,18 +1,18 @@
 def main():
     #escribe tu código abajo de esta línea
     print('to stop the average, type a negative number')
-n=0
-total=0
-c=0
-while True:
-    n=float(input())
-    if n<0:
-        break
+    n=0
+    total=0
+    c=0
+    while True:
+        n=float(input())
+        if n<0:
+            break
     
-    total=total+n
-    c=c+1
-    p=total/c
-    print('you added :'+str(n))
+        total=total+n
+        c=c+1
+        p=total/c
+        print('you added :'+str(n))
     print('the average until now is:'+str(p))
 
         
