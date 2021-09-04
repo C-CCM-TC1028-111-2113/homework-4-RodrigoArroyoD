ad6f8c88a2656082357f9d59dd650cdf55c6f6de
RodrigoArroyoD <88851208+RodrigoArroyoD@users.noreply.github.com>
2021-09-04 12:05:35 -0500

    Update exercise.py

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -2,6 +2,14 @@
 
 def main():
     #Escribe tu código debajo de esta línea
+    n=int(input())
+    s=0
+    while True:
+        if s*s>n:
+            break
+        s=s+1
+    print(s)
+
     pass
 
 if __name__=='__main__':
